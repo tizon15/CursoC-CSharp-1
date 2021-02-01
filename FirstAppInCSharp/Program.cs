ffb04bc4c2f0451d82af8e2a1c98adc262a75e3a
Alvaro Martínez-Tizón <tizon15@gmail.com>
2021-02-01 10:26:59 +0100

    Beginning of the course, implementation of comments

---
+++ FirstAppInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstAppInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comentario simple
+            /* Comentario 
+             * de varias
+             * lineas */
+            Console.WriteLine("Bienvenidos al curso de C#");
+        }
+    }
+}
